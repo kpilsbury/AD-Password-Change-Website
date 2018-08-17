d6226ced7fc8a878269e19a28e71dcd045f3ee2f
kpilsbury <kpilsbury@users.noreply.github.com>
2018-08-17 11:10:25 +1000

    initial change

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.DirectoryServices.AccountManagement;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PWChange2.Models;
+using PWChange2.Classes;
+
+
+namespace PWChange2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() {
+
+            if (TempData["Pass"] == null)
+                TempData["Pass"] = "";
+            if (TempData["Exception"] == null)
+                TempData["Exception"] = "";
+            ViewData["ExceptionMessage"] = TempData["Exception"];
+            ViewData["Pass"] = TempData["Pass"];
+            return View();
+        }
+
+        public IActionResult Complete()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePW([Bind("UserName,CurrentPassword,NewPassword,ConfirmPassword")] User userObject)
+        {
+            //Validating ReCaptcha
+            string EncodedResponse = Request.Form["g-Recaptcha-Response"];
+            bool IsCaptchaValid = (ReCaptchaClass.Validate(EncodedResponse) == "true" ? true : false);
+
+            String displayName = "";
+
+            if (IsCaptchaValid)
+            {
+                if (userObject.confirmNewPwEqualsConfirmPw())
+                {
+                    using (var context = new PrincipalContext(ContextType.Domain))
+                    {
+                        try
+                        {
+                            var user = UserPrincipal.FindByIdentity(context, IdentityType.SamAccountName, userObject.UserName);
+
+                            if (user == null)
+                                throw new Exception("Username does not Exist, Please enter a valid Username");
+
+                            user.ChangePassword(userObject.CurrentPassword, userObject.NewPassword);
+                            displayName = user.Name;
+                            user.Save();
+                        }
+                        catch (PasswordException e)
+                        {
+                            TempData["Exception"] = e.Message.Remove(e.Message.Length - 36);
+                            return RedirectToAction("Index");
+                        }
+                        catch (PrincipalOperationException e)
+                        {
+                            TempData["Exception"] = e.Message.Remove(e.Message.Length - 36);
+                            return RedirectToAction("Index");
+                        }
+                        catch (Exception e)
+                        {
+                            TempData["Exception"] = e.Message;
+                            return RedirectToAction("Index");
+                        }
+                    }
+                }
+            }else
+            {
+                TempData["Exception"] = "Invalid ReCaptcha Response - Please ensure the I'm not a robot check box is ticked";
+                return RedirectToAction("Index");
+            }
+
+            TempData["Pass"] = "Password Changed for " + displayName;
+            return RedirectToAction("Index");
+        }
+
+
+
+    public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+
+
+}
---
+++ Models/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PWChange2.Models
+{
+    public partial class User
+    {
+        [Required(ErrorMessage = "Required")]
+        [Display(Name = "User Name")]
+        public String UserName { get; set; }
+        [Required(ErrorMessage = "Required")]
+        [Display(Name = "Current Password")]
+        [DataType(DataType.Password)]
+        public String CurrentPassword { get; set; }
+        [Required(ErrorMessage = "Required")]
+        [MinLength(8, ErrorMessage = "8 Caracters or more must be entered into this field")]
+        [RegularExpression("(?=.*[A-Z])(?=.*\\d)(?!.*(.)\\1\\1)[a-zA-Z0-9_!./#$\\*]{0,30}$", ErrorMessage = "Password Does Not meet complexity Requirements")]
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        public String NewPassword { get; set; }
+        [Required(ErrorMessage = "Required")]
+        [MinLength(8, ErrorMessage = "8 Caracters or more must be entered into this field")]
+        [Compare("NewPassword", ErrorMessage = "New Password and Confirm Password Fields are not the same, please re-enter")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public String ConfirmPassword { get; set; }
+
+
+
+
+        public Boolean confirmNewPwEqualsConfirmPw()
+        {
+            if (this.NewPassword.Equals(this.ConfirmPassword))
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
+
+
